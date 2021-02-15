36aec35eed0cc0ef187ab8a45e9d82864aaffb78
Eden Berhan Alem <edenberhanalem@gmail.com>
2021-02-16 01:23:53 +0300

    Generated a random number between 1 and 100 and keep track of the user's chances in guessing

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,9 @@ const display = document.querySelector('.display');
 
 // Added an onclick listener to the button
 enterGuess.addEventListener('click', enter);
+
+// Generated a random number between 1 and 100
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+// Declared a variable to count the number of chances a user has
+let turns = 1;
